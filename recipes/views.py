01e8c12f5064b9cfe6fbf75c4590d945a302e436
Ivancly Batista da Silva <ivanclysilva@gmail.com>
2023-07-28 21:06:25 -0300

    S7A42 - Uma nova URL para uma nova View

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'nome': 'Luiz Otávio',
+    })
 
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'nome': 'Luiz Otávio',
+    })
